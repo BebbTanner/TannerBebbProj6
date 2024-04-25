fd80e110323f6cdbfea57a762cd64fb06bf7bd05
Tanner Bebb <17tbebb@gmail.com>
2024-04-25 09:38:59 -0700

    Defined the 5 prompts on lines 15 - 19.

--- TannerBebbProj.6/TannerBebbProj6.asm
+++ TannerBebbProj.6/TannerBebbProj6.asm
@@ -4,13 +4,19 @@ INCLUDE Irvine32.inc
 
 .data
 
-Menu BYTE "---- Boolean Calculator ----", 0dh, 0ah
-	 BYTE 0dh, 0ah
-	 BYTE "1.) ", 0dh, 0ah
-	 BYTE "2.) ", 0dh, 0ah
-	 BYTE "3.) ", 0dh, 0ah
-	 BYTE "4.) ", 0dh, 0ah
-	 BYTE "5.) ", 0
+	Menu BYTE "---- Boolean Calculator ----", 0dh, 0ah
+		 BYTE 0dh, 0ah
+		 BYTE "1.) ", 0dh, 0ah
+		 BYTE "2.) ", 0dh, 0ah
+		 BYTE "3.) ", 0dh, 0ah
+		 BYTE "4.) ", 0dh, 0ah
+		 BYTE "5.) ", 0
+
+	ANDPrompt BYTE "Boolean AND", 0
+	ORPrompt BYTE "Boolean OR", 0
+	NOTPrompt BYTE "Boolean NOT", 0
+	XORPrompt BYTE "Boolean XOR", 0
+	ErrorPrompt BYTE "Please input a valid number", 0
 
 .code
 

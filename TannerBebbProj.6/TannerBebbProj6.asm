6be33bc229c17d1ef7f0d2a36f36524962b12824
Tanner Bebb <17tbebb@gmail.com>
2024-04-25 09:35:16 -0700

    Declared and defined the menu variable. This is going to show the user the opitons that they have for this program.

--- TannerBebbProj.6/TannerBebbProj6.asm
+++ TannerBebbProj.6/TannerBebbProj6.asm
@@ -4,6 +4,14 @@ INCLUDE Irvine32.inc
 
 .data
 
+Menu BYTE "---- Boolean Calculator ----", 0dh, 0ah
+	 BYTE 0dh, 0ah
+	 BYTE "1.) ", 0dh, 0ah
+	 BYTE "2.) ", 0dh, 0ah
+	 BYTE "3.) ", 0dh, 0ah
+	 BYTE "4.) ", 0dh, 0ah
+	 BYTE "5.) ", 0
+
 .code
 
 main PROC
